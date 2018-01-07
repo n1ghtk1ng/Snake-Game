3a6661745538394a1b2098aa536170036f20c55f
aman <amanguptaer2309@gmail.com>
2018-01-07 10:35:35 +0530

    added init , draw, update, gameloop functions

---
+++ snake.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Aman on 1/7/2018.
+ */
+// Snake Game
+// game loop --> Init, Draw, Update
+
+function init() {
+    
+}
+
+function draw() {
+    
+}
+
+function update() {
+
+}
+
+function gameLoop() {
+    draw();
+    update();
+
+}
+
+init();
+gameLoop();
